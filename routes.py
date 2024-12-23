331b9981858f523bf00576466bf3ae0c0b8dcd49
Ezequiel <equintana779@gmail.com>
2024-12-23 18:50:07 -0300

    register

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, url_for, flash, redirect
-from app import app, db
+from app import app, db, bcrypt
 from model import User, Task
+from forms import RegistrationForm, LoginForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/")
@@ -12,3 +13,32 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email = form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title = 'Registe', form = form)
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login ():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember = form.remember.data)
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccesful. Please check email and password', 'danger')
+    return render_template('login.html', title = 'Login', form = form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
