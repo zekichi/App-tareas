117a4c92e23ed618ea73d9aaa050f0d59bf63e13
Ezequiel <equintana779@gmail.com>
2024-12-25 11:59:23 -0300

    components

--- task-manager-frontend/src/components/RedisterForm.js
+++ task-manager-frontend/src/components/RedisterForm.js
@@ -25,7 +25,23 @@ const RegisterForm = () => {
         <form onSubmit={handleSubmit}>
             <div>
                 <label>Username</label>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
             </div>
+            <div>
+                <label>Email</label>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <div>
+                <label>Confirm Password</label>
+                <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+            </div>
+            <button type="submit">Register</button>
         </form>
-    )
+    );
 };
+
+export default RegisterForm;
